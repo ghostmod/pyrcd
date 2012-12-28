ec7294c0af960a76247b23e412bff17e907681e3
FaceHunter <michael@famvdnet.nl>
2012-12-28 12:50:52 +0100

    jahwohl

--- server.py
+++ server.py
@@ -20,8 +20,7 @@ class IRCserver(threading.Thread):
 	def __init__(self, (sock, addr)):
 		self.sock = sock
 		self.addr = addr
-		self.userinf = {self.sock:{"nick":"","host":"","address":self.addr,"username":"","realname":"","usermodes":"","channels":[]}}
-		all_clients.append(sock)
+		self.userinf = {"socket":self.sock,"nick":"","host":self.addr[0],"username":"","realname":"","usermodes":"","channels":[]}
 		print addr
 		threading.Thread.__init__(self)
 		self.startup()
@@ -32,11 +31,37 @@ class IRCserver(threading.Thread):
 			text = self.sock.recv(size)
 			print text
 			if text.startswith("NICK"):
-				self.userinf[self.sock]["nick"] = text.split("NICK ")[1].split("\n")[0]
+				nick = text.split('\n')[0]
+				user = text.split('\n')[1]
+				self.nick = nick.split("NICK ")[1].split("\n")[0]
+				self.userinf["nick"] = self.nick
+				self.sock.send(":localhost NICK :"+nick.split("NICK ")[1].split("\n")[0]+"\n")
 				print self.userinf
+			
+				self.username = user.split()[1]
+				self.realname = user.split(":")[1].split("\n")[0]
+				self.userinf["username"] = self.username
+				self.userinf["realname"] = self.realname
+				print self.userinf
+				all_clients.append(self.userinf)
+				self.main()
 			if not text:
 				die()
 
+			
+			
+	def main(self):
+		while 1:
+			text = self.sock.recv(size)
+			print text
+			if text.startswith("JOIN"):
+				channel=text.split("JOIN ")[1]
+				for x in all_clients:
+					print x
+					if x["username"] == self.username:
+						x["channels"].append(channel)
+				self.sock.send(":localhost JOIN :"+channel+"\n")
+			
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, int(port)))
 s.listen(2)
